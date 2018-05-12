b72a064c94da71bf4c20840ad38c6b1ccc1f0a84
Prosper NWEZE <daponextraspp@gmail.com>
2018-05-12 23:52:50 +0100

    Added auth to post routes and fixes a bug with validation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -17,52 +17,54 @@ class PostController extends Controller
     public function __construct()
     {
         //
+        $this->middleware('auth');
     }
 
     //
-    public function index(){
-        $result =  Post::with('user')->get();
+    public function index()
+    {
+        $result = Post::with('user')->get();
         return [
-            'status'=>true,
-            'data'=>$result
+            'status' => true,
+            'data' => $result
         ];
     }
 
-    public function store(Request $request){
-        $data = $request->all(['text','user_id','title']);
+    public function store(Request $request)
+    {
+        $data = $request->all(['text', 'user_id', 'title']);
 
-        $validator = new Validator($data,[
-            'text'=>'required|min:6|string',
-            'title'=>'required|min:2|string',
-            'user_id'=>'exist:user,id|numeric'
+        $validator = new Validator($data, [
+            'text' => 'required|min:6|string',
+            'title' => 'required|min:2|string',
         ]);
-        if($validator->valid()){
+        if (!$validator->fails()) {
             $post = new Post();
             $post->text = $data['text'];
             $post->title = $data['title'];
-            $post->user_id = $data['user_id'];
+            $post->user_id = Auth::user()->id;
             $post->post_id = $data['post_id'];
-            
-            if($post->save()){
+
+            if ($post->save()) {
                 return [
-                    'status'=>true,
-                    'message'=>'Post saved',
-                    'data'=>$post
+                    'status' => true,
+                    'message' => 'Post saved',
+                    'data' => $post
                 ];
 
             }
 
             return [
-                'status'=>false,
-                'message'=>'unable to save post. Please try again'
+                'status' => false,
+                'message' => 'unable to save post. Please try again'
             ];
-        }else{
+        } else {
             //invalid data
 
             return [
-                'status'=>false,
-                'message'=>'Incorrect or incomplete data',
-                'errors'=>$validator->errors()->all()
+                'status' => false,
+                'message' => 'Incorrect or incomplete data',
+                'errors' => $validator->errors()->all()
             ];
         }
     }
